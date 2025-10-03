ede67ad40313361478064e8d029eba565ddce680
Jyothi Surampudi <jsurampudi@newrelic.com>
2025-10-03 14:08:41 +0530

    updated the query by removing system queries

--- receiver/newrelicoraclereceiver/queries/qpm_queries.go
+++ receiver/newrelicoraclereceiver/queries/qpm_queries.go
@@ -39,28 +39,8 @@ const (
 		LEFT JOIN
 			full_scans fs ON sa.sql_id = fs.sql_id
 		WHERE
-			sa.executions > 0
-		ORDER BY
-			avg_elapsed_time_ms DESC
-		FETCH FIRST 10 ROWS ONLY`
-
-	// Original slow queries SQL for backward compatibility
-	SlowQueriesSQLOriginal = `
-		SELECT
-			d.name AS database_name,
-			sql_id AS query_id,
-			parsing_schema_name AS schema_name,
-			module AS statement_type,
-			executions AS execution_count,
-			sql_fulltext AS query_text,
-			cpu_time / DECODE(executions, 0, 1, executions) / 1000 AS avg_cpu_time_ms,
-			disk_reads / DECODE(executions, 0, 1, executions) AS avg_disk_reads,
-			elapsed_time / DECODE(executions, 0, 1, executions) / 1000 AS avg_elapsed_time_ms
-		FROM
-			v$sqlarea,
-			v$database d
-		WHERE
-			executions > 0
+		    sa.parsing_schema_name NOT IN ('SYS', 'SYSTEM')
+			 and sa.executions > 0
 		ORDER BY
 			avg_elapsed_time_ms DESC
 		FETCH FIRST 10 ROWS ONLY`
