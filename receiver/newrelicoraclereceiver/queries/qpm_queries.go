87432681c654372e7f7a58d0a8a3ade3701d9b99
Jyothi Surampudi <jsurampudi@newrelic.com>
2025-10-08 10:56:28 +0530

    updated query

--- receiver/newrelicoraclereceiver/queries/qpm_queries.go
+++ receiver/newrelicoraclereceiver/queries/qpm_queries.go
@@ -47,22 +47,6 @@ const (
 		FETCH FIRST 10 ROWS ONLY`
 
 	// Oracle SQL query for individual queries metrics
-	IndividualQueriesSQL = `
-		SELECT
-		    sql_id as query_id,
-		    sql_text as query_text,
-		    cpu_time/1000 as cpu_time_ms,
-		    elapsed_time / 1000 AS elapsed_time_ms
-		FROM
-		    v$sql
-		WHERE
-		    sql_id is not null
-		ORDER BY
-		    elapsed_time_ms DESC
-		FETCH FIRST 10 ROWS ONLY`
-
-	// Oracle SQL query for individual queries metrics filtered by query IDs
-	// This query uses placeholders for query IDs that will be replaced programmatically
 	IndividualQueriesFilteredSQL = `
 		SELECT
 		    sql_id as query_id,
