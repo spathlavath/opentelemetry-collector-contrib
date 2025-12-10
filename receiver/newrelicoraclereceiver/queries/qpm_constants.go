dffde2bbdaf70bc91c92fa5c77cee2d135531203
tharun0064 <tbalanagu@newrelic.com>
2025-12-10 17:14:38 +0530

    change default configs

--- receiver/newrelicoraclereceiver/queries/qpm_constants.go
+++ receiver/newrelicoraclereceiver/queries/qpm_constants.go
@@ -6,12 +6,11 @@ package queries
 // QPM (Query Performance Monitoring) configuration constants
 const (
 	// Default values used when configuration is not specified
-	DefaultQueryMonitoringResponseTimeThreshold = 500 // milliseconds
-	DefaultQueryMonitoringCountThreshold        = 20  // queries
+	DefaultQueryMonitoringResponseTimeThreshold = 100 // milliseconds
+	DefaultQueryMonitoringCountThreshold        = 20  
 
 	// Validation ranges for configuration values
-	MinQueryMonitoringResponseTimeThreshold = 0    // minimum realistic threshold for Oracle
-	MaxQueryMonitoringResponseTimeThreshold = 5000 // practical limit for OLTP workloads
-	MinQueryMonitoringCountThreshold        = 10   // prevents too little data collection
-	MaxQueryMonitoringCountThreshold        = 50   // performance limit for concurrent queries
+	MinQueryMonitoringResponseTimeThreshold = 0  
+	MinQueryMonitoringCountThreshold        = 20 
+	MaxQueryMonitoringCountThreshold        = 50
 )
