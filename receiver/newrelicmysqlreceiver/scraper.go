3933c6aed20798391c6c4dd1e952feec54b22df6
sairaj18 <sairajsunny18@gmail.com>
2026-01-09 22:11:14 +0530

    feat(newrelicmysql): execute basic query and populate metrics

--- receiver/newrelicmysqlreceiver/scraper.go
+++ receiver/newrelicmysqlreceiver/scraper.go
@@ -6,10 +6,13 @@ package newrelicmysqlreceiver // import "github.com/open-telemetry/opentelemetry
 import (
 	"context"
 	"errors"
+	"time"
 
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/scraper/scrapererror"
 	"go.uber.org/zap"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/newrelicmysqlreceiver/internal/metadata"
@@ -63,15 +66,59 @@ func (n *newRelicMySQLScraper) scrape(ctx context.Context) (pmetric.Metrics, err
 		return pmetric.Metrics{}, errors.New("failed to connect to database client")
 	}
 
-	// TODO: Implement metric collection logic here
-	// Example:
-	// stats, err := n.sqlclient.getGlobalStats()
-	// if err != nil {
-	//     n.logger.Error("Failed to fetch global stats", zap.Error(err))
-	//     return pmetric.Metrics{}, err
-	// }
+	now := pcommon.NewTimestampFromTime(time.Now())
+	errs := &scrapererror.ScrapeErrors{}
 
-	n.logger.Info("Scraping New Relic MySQL metrics")
+	// collect global status metrics.
+	n.logger.Info("Scraping MySQL metrics using newrelicmysql receiver started")
+	n.scrapeGlobalStats(now, errs)
+	n.logger.Info("Scraping MySQL metrics using newrelicmysql receiver completed")
 
 	return n.mb.Emit(), nil
+}
+
+func (m *newRelicMySQLScraper) scrapeGlobalStats(now pcommon.Timestamp, errs *scrapererror.ScrapeErrors) {
+	globalStats, err := m.sqlclient.getGlobalStats()
+	if err != nil {
+		m.logger.Error("Failed to fetch global stats", zap.Error(err))
+		errs.AddPartial(66, err)
+		return
+	}
+
+	for k, v := range globalStats {
+		switch k {
+
+		// connection
+		case "Connections":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlConnectionCountDataPoint(now, v))
+
+		// commands
+		case "Com_delete":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlCommandsDataPoint(now, v, metadata.AttributeCommandDelete))
+		case "Com_delete_multi":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlCommandsDataPoint(now, v, metadata.AttributeCommandDeleteMulti))
+		case "Com_insert":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlCommandsDataPoint(now, v, metadata.AttributeCommandInsert))
+		case "Com_select":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlCommandsDataPoint(now, v, metadata.AttributeCommandSelect))
+		case "Com_update":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlCommandsDataPoint(now, v, metadata.AttributeCommandUpdate))
+		case "Com_update_multi":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlCommandsDataPoint(now, v, metadata.AttributeCommandUpdateMulti))
+
+		// queries
+		case "Queries":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlQueryCountDataPoint(now, v))
+
+		// uptime
+		case "Uptime":
+			addPartialIfError(errs, m.mb.RecordNewrelicmysqlUptimeDataPoint(now, v))
+		}
+	}
+}
+
+func addPartialIfError(errors *scrapererror.ScrapeErrors, err error) {
+	if err != nil {
+		errors.AddPartial(1, err)
+	}
 }
