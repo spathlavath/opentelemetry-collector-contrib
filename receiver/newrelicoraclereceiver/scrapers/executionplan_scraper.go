032cd3ad2af59c05af154a76b7a8e662eff85019
tharun0064 <tbalanagu@newrelic.com>
2026-01-28 11:00:59 +0530

    test

--- receiver/newrelicoraclereceiver/scrapers/executionplan_scraper.go
+++ receiver/newrelicoraclereceiver/scrapers/executionplan_scraper.go
@@ -285,11 +285,15 @@ func (s *ExecutionPlanScraper) cleanupCache(now time.Time) {
 	s.cacheMutex.Lock()
 	defer s.cacheMutex.Unlock()
 
+	s.logger.Debug("Cache state before cleanup",
+		zap.Int("total_entries", len(s.cache)))
+
 	var keysToDelete []string
 	for key, entry := range s.cache {
 		s.logger.Debug("cached details plan hash",
-				zap.String("plan_hash", now.Sub(entry.lastScraped).String()),
-				zap.String("sql_id", s.cacheTTL.String()))
+			zap.String("plan_hash", key),
+			zap.String("cache_age", now.Sub(entry.lastScraped).String()),
+			zap.String("ttl", s.cacheTTL.String()))
 		if now.Sub(entry.lastScraped) >= s.cacheTTL {
 			keysToDelete = append(keysToDelete, key)
 		}
